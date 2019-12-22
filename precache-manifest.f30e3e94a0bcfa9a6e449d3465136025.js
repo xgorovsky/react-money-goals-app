8997cde35c500762261beca956c0daec9020e5ec
xgorovsky <igorbiedrawa@gmail.com>
2019-12-22 12:33:52 +0100

    Updates

---
+++ precache-manifest.f30e3e94a0bcfa9a6e449d3465136025.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "222a1540d947fd93ca6fa5adc1233210",
+    "url": "/react-money-goals-app/index.html"
+  },
+  {
+    "revision": "3b019efa42d41edc69d0",
+    "url": "/react-money-goals-app/static/css/main.47dee5f1.chunk.css"
+  },
+  {
+    "revision": "011c0b1f84a4e966e24b",
+    "url": "/react-money-goals-app/static/js/2.e67e174f.chunk.js"
+  },
+  {
+    "revision": "c6f2a8cff5bda75ce9d9d2e5dad3a2a7",
+    "url": "/react-money-goals-app/static/js/2.e67e174f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3b019efa42d41edc69d0",
+    "url": "/react-money-goals-app/static/js/main.a77dcc37.chunk.js"
+  },
+  {
+    "revision": "d47cf52b9b44489a7717",
+    "url": "/react-money-goals-app/static/js/runtime-main.4c21d361.js"
+  },
+  {
+    "revision": "88642987d9e0b6fdff529eef78614aad",
+    "url": "/react-money-goals-app/static/media/clothes.88642987.svg"
+  },
+  {
+    "revision": "1e4b5790d792df2c8aa916157cafaa87",
+    "url": "/react-money-goals-app/static/media/electronics.1e4b5790.svg"
+  },
+  {
+    "revision": "2d71706aad40468d5c0effb1ae5ea7ca",
+    "url": "/react-money-goals-app/static/media/estate.2d71706a.svg"
+  },
+  {
+    "revision": "0e8afb5c697b412bf2b8b61f3ba0640d",
+    "url": "/react-money-goals-app/static/media/header_logo.0e8afb5c.svg"
+  },
+  {
+    "revision": "71cbc34a7ad718eb4569f7c61ff5b37b",
+    "url": "/react-money-goals-app/static/media/money-blue.71cbc34a.png"
+  },
+  {
+    "revision": "d5b56ecc9e1340649926a8418fb940be",
+    "url": "/react-money-goals-app/static/media/other.d5b56ecc.svg"
+  },
+  {
+    "revision": "a3381042aab9efde7adf27b377bc6e22",
+    "url": "/react-money-goals-app/static/media/trip.a3381042.svg"
+  },
+  {
+    "revision": "8e70b48d684d3b7b33848b0ef22f6685",
+    "url": "/react-money-goals-app/static/media/undraw_1.8e70b48d.svg"
+  },
+  {
+    "revision": "3e6d9686a2404281e9e9a3b0e6460c19",
+    "url": "/react-money-goals-app/static/media/undraw_2.3e6d9686.svg"
+  },
+  {
+    "revision": "cae414952dc640865b65272d62c965a6",
+    "url": "/react-money-goals-app/static/media/undraw_3.cae41495.svg"
+  },
+  {
+    "revision": "cfb4e3ea73172350388ee3f363cece04",
+    "url": "/react-money-goals-app/static/media/vehicle.cfb4e3ea.svg"
+  }
+]);
